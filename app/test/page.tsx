2d1c1df6bc0c80f54da6397d24f84f613fc086a9
Alex <cawasp@gmail.com>
2023-11-15 17:07:23 -0800

    radix ui select test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,14 +1,37 @@
 'use client'
 
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-
+// import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import * as Select from '@radix-ui/react-select';
 export default function page() {
   return (
-  <div className="relative mx-auto m-2 w-[500px]">
-    <span className="absolute w-[125px] h-[25px] bg-lightbg bottom-3.5 left-1/2 -translate-x-1/2 rounded"/>
-    <PayPalScriptProvider options={{clientId: "test"}}>
-      <PayPalButtons style={{layout: "vertical"}}/>
-    </PayPalScriptProvider>
-  </div>
+    <Select.Root>
+    <Select.Trigger>
+      <Select.Value placeholder="Select something" />
+      <Select.Icon />
+    </Select.Trigger>
+
+    <Select.Portal>
+      <Select.Content>
+        <Select.Viewport>
+          <Select.Item value="test">
+            <Select.ItemText >test</Select.ItemText>
+            <Select.ItemIndicator />
+          </Select.Item>
+
+          <Select.Item value="test2">
+            <Select.ItemText >test2</Select.ItemText>
+            <Select.ItemIndicator />
+          </Select.Item>
+
+        </Select.Viewport>
+      </Select.Content>
+    </Select.Portal>
+  </Select.Root>
+  // <div className="relative mx-auto m-2 w-[500px]">
+  //   <span className="absolute w-[125px] h-[25px] bg-lightbg bottom-3.5 left-1/2 -translate-x-1/2 rounded"/>
+  //   <PayPalScriptProvider options={{clientId: "test"}}>
+  //     <PayPalButtons style={{layout: "vertical"}}/>
+  //   </PayPalScriptProvider>
+  // </div>
   )
 }
