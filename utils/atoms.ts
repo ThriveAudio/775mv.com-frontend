e9392093578ab80fa94358a0fc5394924fd2221b
Alex <cawasp@gmail.com>
2023-12-27 10:43:40 -0800

    atom for back navigation after auth

--- utils/atoms.ts
+++ utils/atoms.ts
@@ -3,3 +3,4 @@ import { atom } from 'jotai'
 export const cartAtom = atom(0)
 export const cartScrollAtom = atom(false)
 export const sessionAtom = atom("unknown")
+export const pageBackAtom = atom(false)
