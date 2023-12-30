759e0571418e734174c0f2a948207a75fcdb114a
Alex <cawasp@gmail.com>
2023-12-30 12:58:04 -0800

    registration finally works properly

--- app/account/register/page_client.tsx
+++ app/account/register/page_client.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation'
 import { useRef, useReducer, useState } from "react"
 import { useAtom } from 'jotai'
 import { pageBackAtom } from '../../../utils/atoms'
+import Link from 'next/link'
 
 function registerReducer(items, action) {
   switch (action.type) {
@@ -60,29 +61,25 @@ export default function RegisterClient({redirect, trustedDevice}) {
     })
   }
 
-  async function handleSubmit() {
-    const res = await (await fetch('/api/register', {"method": "post", "body": JSON.stringify({
-      items
+  async function checkAccountExists() {
+    const email = items.email
+    const res = await (await fetch('/api/check-account-exists', {"method": "post", "body": JSON.stringify({
+      email
     })})).json()
+    setAccountExists(res)
+    return res
+  }
 
-    if (res['result'] == "redirect") {
-      setPageBack(true)
-      router.back()
-    } else if(res['result'] == "error") {
-      setAccountExists(true)
-    } else if(res['result'] == "password") {
-      let classList = refs.password.current.className.split(" ")
-      classList.push("!border-burgundy")
-      refs.password.current.className = classList.join(" ")
-      setHighlightPasswordInfo(true)
+  async function handleSubmit() {
+    if (!(await checkAccountExists())) {
+      confirmEmail()
     }
   }
 
-  async function handleConfirmEmail() {
+  async function confirmEmail() {
     const res = await (await fetch('/api/confirm-email', {"method": "post", "body": JSON.stringify({
       "email": refs.email.current.value
     })})).json()
-    console.log(res)
     if (res.result == "error") {
       let classList = refs.email.current.className.split(" ")
       classList.push("!border-burgundy")
@@ -90,6 +87,7 @@ export default function RegisterClient({redirect, trustedDevice}) {
       return
     } else if (res.result == "confirmed") {
       setEmailConfirmed(true)
+      register()
       return
     }
 
@@ -103,10 +101,29 @@ export default function RegisterClient({redirect, trustedDevice}) {
           clearInterval(emailIntervalRef)
           setAwaitingEmail(false)
           setEmailConfirmed(true)
+          register()
         }
       })
 
     }, 3000)
+  }
+
+  function register() {
+    fetch('/api/register', {"method": "post", "body": JSON.stringify({
+      items
+    })}).then((e)=>e.json()).then(e=> {
+      if (e['result'] == "redirect") {
+        setPageBack(true)
+        router.back()
+      } else if(e['result'] == "error") {
+        setAccountExists(true)
+      } else if(e['result'] == "password") {
+        let classList = refs.password.current.className.split(" ")
+        classList.push("!border-burgundy")
+        refs.password.current.className = classList.join(" ")
+        setHighlightPasswordInfo(true)
+      }
+    })
   }
 
   // console.log(emailConfirmed)
@@ -119,11 +136,7 @@ export default function RegisterClient({redirect, trustedDevice}) {
     })
   }
 
-  if (!emailConfirmed) {
-    passwordInfo = <p className='w-[211px] text-xs text-amber/30'>
-      The password must have at least 1 of upper and lower case letters, numbers, and special characters.
-    </p>
-  } else if (highlightPasswordInfo) {
+  if (highlightPasswordInfo) {
     passwordInfo = <p className='w-[211px] text-xs text-burgundy'>
       The password must have at least 1 of upper and lower case letters, numbers, and special characters.
     </p>
@@ -142,30 +155,25 @@ export default function RegisterClient({redirect, trustedDevice}) {
         <input ref={refs.email} onInput={() => handleInputUpdate("email")} value={items['email']} placeholder="Email" autoComplete="email" className="m-2 w-[211px] border-2 border-coolgraylight focus:border-ochre focus:outline-none rounded-lg bg-coolgraymid p-1 placeholder:text-lightoutline"/>
         {
           awaitingEmail ?
-          <p>We sent you a confirmation email. Please check your inbox.</p>
+          <p className='w-[211px] text-sm'>We sent you a confirmation email. Please check your inbox.</p>
           :
           <></>
         }
-        {
+        {/* {
           emailConfirmed ?
           <></>
           :
           <button onClick={handleConfirmEmail} className="m-2 w-[211px] border-2 border-ochre rounded-lg p-1 bg-amber text-coolgraydark font-bold hover:shadow-[0px_5px_10px_0px_rgba(0,0,0,1)] hover:scale-105 active:scale-[102%] active:shadow-[0px_1px_5px_0px_rgba(0,0,0,1)]">
             Confirm Email
           </button>
-        }
-        <input disabled={!emailConfirmed} ref={refs.password} onInput={() => handleInputUpdate("password")} value={items['password']} placeholder="Password" autoComplete="password" type="password" className="m-2 w-[211px] border-2 border-coolgraylight focus:border-ochre focus:outline-none rounded-lg bg-coolgraymid p-1 placeholder:text-lightoutline disabled:placeholder:text-coolgraylight disabled:border-coolgraylight/30"/>
+        } */}
+        <input ref={refs.password} onInput={() => handleInputUpdate("password")} value={items['password']} placeholder="Password" autoComplete="password" type="password" className="m-2 w-[211px] border-2 border-coolgraylight focus:border-ochre focus:outline-none rounded-lg bg-coolgraymid p-1 placeholder:text-lightoutline disabled:placeholder:text-coolgraylight disabled:border-coolgraylight/30"/>
         {passwordInfo}
         <div className="m-1 w-[211px]">
-            <input disabled={!emailConfirmed} onClick={handleCheck} type="checkbox" checked={items.check}/>
-            {
-              emailConfirmed ?
-              <label className="ml-2">Trust this device</label>
-              :
-              <label className="ml-2 text-amber/30">Trust this device</label>
-            }
+            <input onClick={handleCheck} type="checkbox" checked={items.check}/>
+            <label className="ml-2">Trust this device</label>
         </div>
-        <button disabled={!emailConfirmed} onClick={handleSubmit} className="m-2 w-[211px] border-2 border-ochre rounded-lg bg-amber p-1 font-bold text-coolgraydark hover:shadow-[0px_5px_10px_0px_rgba(0,0,0,1)] hover:scale-105 active:scale-[102%] active:shadow-[0px_1px_5px_0px_rgba(0,0,0,1)] disabled:border-ochre/[.01] disabled:bg-ochre/50 disabled:active:shadow-none disabled:hover:shadow-none disabled:hover:scale-100">
+        <button onClick={handleSubmit} className="m-2 w-[211px] border-2 border-ochre rounded-lg bg-amber p-1 font-bold text-coolgraydark hover:shadow-[0px_5px_10px_0px_rgba(0,0,0,1)] hover:scale-105 active:scale-[102%] active:shadow-[0px_1px_5px_0px_rgba(0,0,0,1)]">
           Create Account
         </button>
         {
@@ -174,9 +182,12 @@ export default function RegisterClient({redirect, trustedDevice}) {
             <div className='mt-3'>
               This account already exists.
             </div>
-            <button className='mt-2 border-2 border-coolgraylight py-1 rounded-lg bg-coolgraymid hover:border-ochre'>
+            <Link href="/account/login" className='mt-2 border-2 border-coolgraylight py-1 text-center rounded-lg bg-coolgraymid hover:border-ochre'>
+              Login
+            </Link>
+            <Link href="/account/reset-password" className='mt-2 border-2 border-coolgraylight py-1 text-center rounded-lg bg-coolgraymid hover:border-ochre'>
               Reset Password
-            </button> {/* TODO make the reset password button work */}
+            </Link> {/* TODO make the reset password button work */}
           </div>
           :
           <></>
