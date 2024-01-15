27c87d453a36ecc2057d0333ef55ca0f484abfb1
Alex <cawasp@gmail.com>
2024-01-14 22:26:20 -0800

    clean up

--- app/products/page_client.tsx
+++ app/products/page_client.tsx
@@ -19,16 +19,11 @@ function Categories({categories, current, onclick}) {
   return (
     <div className='fixed w-30 mt-5 flex flex-col items-start'>
       {buttons}
-      {/* <button className='w-[110px] text-left pl-3 -ml-[1px] border-l hover:bg-gray-100 border-l-gray-500'>All</button>
-      <button className='w-[110px] text-left pl-3 -ml-[1px] border-l hover:bg-gray-100'>Filters</button>
-      <button className='w-[110px] text-left pl-3 -ml-[1px] border-l hover:bg-gray-100'>Services</button> */}
     </div>
   )
 }
 
 function Product({product = {}, image = ""}: {product: Object, image: String}) {
-  const imgClass = "bg-[url('" + product['image'] + "')] relative flex justify-center items-center w-[300px] h-[200px] border-b-2 border-b-lightoutline dark:border-b-ochre rounded-t-[7px] overflow-hidden"
-  //console.log(imgClass)
   if (Object.keys(product).length === 0) {
     return (
       <div className='h-[300px] w-[300px] m-3 border rounded-lg shadow-md flex flex-col items-center'>
@@ -41,12 +36,10 @@ function Product({product = {}, image = ""}: {product: Object, image: String}) {
       </div>
     )
   } else {
-    //et image = fs.readdirSync('./public/'+product['sku'])[0]
     return (
       <Link href={"product/"+product['sku']} key={product['_id']} className='interactable group h-[300px] w-[300px] m-3 border border-lightoutline bg-lightaccentbg dark:border-coolgraylight dark:bg-coolgraydark rounded-lg shadow-md dark:shadow-[0_4px_6px_-1px_rgba(0,0,0,1)] hover:shadow-2xl dark:hover:shadow-[0_25px_50px_-12px_rgba(0,0,0,1)] hover:scale-105 transition duration-100 flex flex-col items-center dark:text-white overflow-hidden'>
         <div className="relative flex justify-center items-center w-[300px] h-[200px] border-b-2 border-b-lightoutline dark:border-b-ochre rounded-t-[7px] overflow-hidden">
           <Image src={product['image']} width={300} height={200} alt={product['name']}></Image>
-          {/* <div className='w-[300px] h-[200px] group-hover:scale-110 transition'></div> */}
         </div>
         <div className='mt-3 text-xl text-amber text-center group-hover:underline underline-offset-2 decoration-[1.5px]'>{product['name']}</div>
         <div className='mt-3 text-xl font-semibold'>${product['price']}</div>
@@ -65,16 +58,6 @@ function Products({products}) {
   return (
     <div className='flex flex-row flex-wrap justify-center'>
       {spam}
-    </div>
-  )
-}
-
-function Loading() {
-  return (
-    <div className='animate-pulse ml-[200px] flex flex-row flex-wrap'>
-      <Product/>
-      <Product/>
-      <Product/>
     </div>
   )
 }
