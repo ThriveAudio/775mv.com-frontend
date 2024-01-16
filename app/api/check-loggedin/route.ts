1f47fdd07e25f05cdce2a24fe3f6af0b920a03e6
Alex <cawasp@gmail.com>
2024-01-15 16:40:44 -0800

    [refactor] Export mongodb logic
    Create separate mongodb handler to use for all server routes

--- app/api/check-loggedin/route.ts
+++ app/api/check-loggedin/route.ts
@@ -1,7 +1,6 @@
 // @ts-nocheck
 import { cookies } from 'next/headers'
-const {MongoClient} = require('mongodb');
-const url = 'mongodb://localhost:27017';
+import { getDocument } from '../mongo';
 
 export async function GET(request: Request) {
   console.log('GET check-loggedin Nextjs API called')
@@ -13,12 +12,7 @@ export async function GET(request: Request) {
 
   const sessionId = cookies().get('sessionId')?.value
 
-  const client = new MongoClient(url)
-
-  const db = client.db('775mv_dev');
-  const collection = db.collection('sessions');
-  const session = collection.findOne({"id": sessionId})
+  const session = getDocument("sessions", {"id": sessionId})
   
   return Response.json({result: session['state'] == "loggedin"})
 }
-// TODO export mongodb base logic
--- app/api/get-products/route.ts
+++ app/api/get-products/route.ts
@@ -1,30 +1,11 @@
 // @ts-nocheck
 import { cookies } from 'next/headers'
-const {MongoClient} = require('mongodb');
-const url = 'mongodb://localhost:27017';
+import {getCollectionAsArray} from '../mongo'
 
 export async function GET(request: Request) {
   console.log('GET get-products Nextjs API called')
-  let res = []
 
-  const client = new MongoClient(url)
-
-  const db = client.db('775mv_dev');
-  const collection = db.collection('products');
-  const cursor = collection.find()
-
-
-  let i = 0
-  for await (const doc of cursor) {
-    console.dir(doc);
-    doc['id'] = i
-    res.push(doc)
-    console.log("GET 1 loop")
-    i++
-  }
-
-  // client.close();
+  const res = await getCollectionAsArray('products')
 
   return Response.json(res)
 }
-// TODO export mongodb base logic
