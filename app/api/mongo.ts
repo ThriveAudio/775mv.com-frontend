9a616c1c3c89e089d9bf134f233a61a815d0fc73
Alex <cawasp@gmail.com>
2024-01-15 18:20:54 -0800

    [fix] Make mongodb function async

--- app/api/mongo.ts
+++ app/api/mongo.ts
@@ -4,8 +4,8 @@ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url)
 const db = client.db('775mv_dev');
 
-export function getDocument(collection: string, doc: Map<string, string>) {
-  const dbColl = db.collection(collection);
+export async function getDocument(collection: string, doc: Map<string, string>) {
+  const dbColl = await db.collection(collection);
   return dbColl.findOne(doc)
 }
 
