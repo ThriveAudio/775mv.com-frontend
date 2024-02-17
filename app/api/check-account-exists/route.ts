29a787f5bab39a835660b3cea2f937df66753e87
Alex <cawasp@gmail.com>
2024-02-16 22:46:51 -0800

    Refactor check-account-exists from python to js

--- app/api/check-account-exists/route.ts
+++ app/api/check-account-exists/route.ts
@@ -1,9 +1,17 @@
 import { cookies } from 'next/headers'
+import { getCollectionAsArray } from '../mongo'
 
 export async function POST(request: Request) {
   console.log('POST check-account-exists Nextjs API called')
   const res = await request.json()
-  res['sessionId'] = cookies().get('sessionId')?.value
-  const req = await (await fetch('http://127.0.0.1:8000/check-account-exists', {"method": "post", "body": JSON.stringify(res)})).json()
-  return Response.json(req)
+  // res['sessionId'] = cookies().get('sessionId')?.value
+  // const req = await (await fetch('http://127.0.0.1:8000/check-account-exists', {"method": "post", "body": JSON.stringify(res)})).json()
+  const accounts = await getCollectionAsArray('accounts')
+  for (let i = 0; i < accounts.length; i++) {
+    const account = accounts[i]
+    if (account.email == res['email'] && account.password.length != 0) {
+      return Response.json(true)
+    }
+  }
+  return Response.json(false)
 }
