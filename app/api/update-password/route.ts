42c16f0a97b00eb25173aa9e6d93e692196c3f96
Alex <cawasp@gmail.com>
2024-02-19 21:57:23 -0800

    Refactor update-password

--- app/api/update-password/route.ts
+++ app/api/update-password/route.ts
@@ -1,9 +1,30 @@
+// @ts-nocheck
+
 import { cookies } from 'next/headers'
+import { getDocument, updateDocument } from '../mongo'
+import { mongoId, validatePassword } from '../utils'
+let bcrypt = require('bcryptjs')
 
 export async function POST(request: Request) {
   console.log('POST update password Nextjs API called')
   const res = await request.json()
-  res['sessionId'] = cookies().get('sessionId')?.value
-  const req = await (await fetch('http://127.0.0.1:8000/update-password', {"method": "post", "body": JSON.stringify(res)})).json()
-  return Response.json(req)
+  const sessionId = cookies().get('sessionId')?.value
+  // const req = await (await fetch('http://127.0.0.1:8000/update-password', {"method": "post", "body": JSON.stringify(res)})).json()
+
+  const session = await getDocument('sessions', {'id': sessionId})
+  const account = await getDocument('accounts', {'_id': mongoId(session['account'])})
+
+  const old_hash = bcrypt.hashSync(res.items.oldpassword, account.salt)
+  if (old_hash != account['password']) {
+    return Response.json({'result': 'error'})
+  } else {
+    if (!validatePassword(res.items.newpassword)) {
+      return Response.json({'result': 'error'})
+    }
+  }
+
+  await updateDocument('accounts', {'_id': mongoId(session['account'])}, {'password': bcrypt.hashSync(res.items.newpassword, account.salt)})
+  return Response.json({'result': 'success'})
+
+  // return Response.json(req)
 }
