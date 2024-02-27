e9b7f6f3c3a8b075bcbf5e34ee6951e4148ac3d0
Alex <cawasp@gmail.com>
2024-02-26 20:56:11 -0800

    Refactor keep-alive

--- app/api/keep-alive/route.ts
+++ app/api/keep-alive/route.ts
@@ -1,11 +1,22 @@
 // @ts-nocheck
 import { cookies } from 'next/headers'
+import { getDocument, updateDocument } from '../mongo'
 
 export async function GET(request: Request) {
-  console.log('GET check-loggedin Nextjs API called')
-  const res = {}
-  console.log("GOT HERE")
-  res['sessionId'] = cookies().get('sessionId')?.value
-  const req = await (await fetch('http://127.0.0.1:8000/keep-alive', {"method": "post", "body": JSON.stringify(res)})).json()
-  return Response.json(req)
+  console.log('GET keep-alive Nextjs API called')
+  // const res = {}
+  // res['sessionId'] = cookies().get('sessionId')?.value
+  // const req = await (await fetch('http://127.0.0.1:8000/keep-alive', {"method": "post", "body": JSON.stringify(res)})).json()
+  // return Response.json(req)
+  const sessionId = cookies().get('sessionId')?.value
+  const session = await getDocument('sessions', {'id': sessionId})
+
+  if (!session['trusted_device']) {
+    if (Date.now()/1000 < session['expiration']) {
+      const config = await getDocument('config', {'type': 'config'})
+      await updateDocument('sessions', {'id': sessionId}, {'expiration': Date.now()/1000 + config['short_session']})
+    }
+  }
+
+  return Response.json({})
 }
