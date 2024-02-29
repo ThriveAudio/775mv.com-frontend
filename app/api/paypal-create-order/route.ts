3a8d58a2b74f94a3d4d891c35850bb3bd1922cda
Alex <cawasp@gmail.com>
2024-02-28 22:02:02 -0800

    Refactor paypal-create-order

--- app/api/paypal-create-order/route.ts
+++ app/api/paypal-create-order/route.ts
@@ -1,7 +1,15 @@
+// @ts-nocheck
 import { cookies } from 'next/headers'
+import { getDocument, updateDocument } from '../mongo'
+import { randint } from '../utils'
 
 export async function GET() {
   console.log('POST paypal-create-order Nextjs API called')
-  const req = await (await fetch('http://127.0.0.1:8000/paypal-create-order')).json()
-  return Response.json(req)
+  // const req = await (await fetch('http://127.0.0.1:8000/paypal-create-order')).json()
+  // return Response.json(req)
+  // TODO change last order id to config
+  const last_id = (await getDocument('orders', {'type': 'last_id'}))['id']
+  const new_id = last_id + randint(13)
+  await updateDocument('orders', {'type': 'last_id'}, {'id': new_id})
+  return Response.json(new_id)
 }
