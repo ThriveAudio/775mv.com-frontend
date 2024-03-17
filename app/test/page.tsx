06bd686da588c0ca562343de375f69cdcc029c0f
Alex <cawasp@gmail.com>
2024-03-17 12:00:59 -0700

    Test image stuff

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
+import Image from "next/image"
 import { Suspense } from "react"
+import product from './../../public/product_edited.svg'
 
 // import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 export default function page() {
-  fetch("/api/get-products")
+  // fetch("/api/get-products")
   return (
-    <Suspense>
-      
-    </Suspense>
+    <Image width={400} height={400} alt={"product img"} src={product} className="bg-transparent"></Image>
   )
 }
